b9b9679054c4965b20d19862c2482eb5beb38cb0
wolfbook22 <wolfbook22@hotmail.com>
2025-05-01 09:23:02 -0400

    Schema updated
    Matching Database, Table Names. Auto Increment added to creation.

--- database/JJMTTrailDBSetup.sql
+++ database/JJMTTrailDBSetup.sql
@@ -5,27 +5,29 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema jjmtdb
+-- Schema county_parks
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `jjmtdb` DEFAULT CHARACTER SET utf8 ;
-USE `jjmtdb` ;
+CREATE SCHEMA IF NOT EXISTS `county_parks` DEFAULT CHARACTER SET utf8 ;
+USE `county_parks` ;
 
 -- -----------------------------------------------------
--- Table `jjmtdb`.`trail_reports`
+-- Table `county_parks`.`Incidents`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `jjmtdb`.`trail_reports` (
-  `park` VARCHAR(45) NOT NULL,
-  `trail` VARCHAR(45) NOT NULL,
-  `Date`  datetime(6) NOT NULL,
-  `category` VARCHAR(45) NULL,
+CREATE TABLE IF NOT EXISTS `county_parks`.`Incidents` (
+  `incidentNum` int NOT NULL AUTO_INCREMENT,
+  `ParkName` VARCHAR(45) NOT NULL,
+  `TrailName` VARCHAR(45) NOT NULL,
+  `SubmiterName` VARCHAR(45) NOT NULL,
+  `Contact` VARCHAR(45) NOT NULL,
+  `Date`  DATETIME(6) NOT NULL,
+  `Category` VARCHAR(45) NULL,
+  `Description` VARCHAR(1000) NULL,
   `PhotoURL` VARCHAR(45) NULL,
   `Status` VARCHAR(45) NULL,
   `StaffAssign` VARCHAR(45) NULL,
   `Priority` VARCHAR(45) NULL,
-  `submitername` VARCHAR(45) NOT NULL,
-  `Contact` VARCHAR(45) NULL,
-  `description` VARCHAR(1000) NULL,
-  PRIMARY KEY (`park`, `trail`, `Date`, `submitername`)
+
+  PRIMARY KEY (`incidentNum`)
 ) ENGINE = InnoDB;
 
 SET SQL_MODE=@OLD_SQL_MODE;
@@ -33,8 +35,8 @@ SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 -- Safe insert to avoid duplicates
-INSERT IGNORE INTO `jjmtdb`.`Incidents` 
-(`incidentNum`, `ParkName`, `TrailName`, `Date`, `Category`, `Description`, `Status`, `Priority`)
+INSERT IGNORE INTO `county_parks`.`Incidents` 
+(`incidentNum`, `ParkName`, `TrailName`, `SubmiterName`, `Contact`, `Date`, `Category`, `Description`, `Status`, `Priority`)
 VALUES 
-('001', 'Cromwell Valley', 'Lime Kiln', '2025-01-22', 'Erosion', 'Half of the trail is washed into the stream near the beaver dam', 'New', 'New'),
-('002', 'Marshy Point', 'White Tail', '2025-03-15', 'Obstruction', 'Fallen Tree', 'New', 'New');
+('001', 'Cromwell Valley', 'Lime Kiln', 'Joe', '443-555-5555', '2025-01-22 10:22:35', 'Erosion', 'Half of the trail is washed into the stream near the beaver dam', 'New', 'New'),
+('002', 'Marshy Point', 'White Tail', 'James', '443-555-5555', '2025-03-15 11:45:19', 'Obstruction', 'Fallen Tree', 'New', 'New');
