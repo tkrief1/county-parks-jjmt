23cacb9eb61b5e2a878b7e6522c05ccbcfd253e9
wolfbook22 <wolfbook22@hotmail.com>
2025-05-01 09:13:14 -0400

    Auto Increment

--- database/updateTable.sql
+++ database/updateTable.sql
@@ -1,5 +1,6 @@
 -- Update the Docker MySQL database
-ALTER TABLE `county_parks`.`Incidents` 
+ALTER TABLE `county_parks`.`Incidents` AUTO_INCREMENT = 3;
+ALTER TABLE `county_parks`.`Incidents`
 ADD COLUMN `SubmiterName` VARCHAR(45) NULL AFTER `TrailName`,
 ADD COLUMN `Contact` VARCHAR(45) NULL AFTER `SubmiterName`,
 CHANGE COLUMN `Date` `Date` DATETIME(6) NULL DEFAULT NULL ;
