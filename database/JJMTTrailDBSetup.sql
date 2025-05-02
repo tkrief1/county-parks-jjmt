ec712e0d7328ae818de6ce9e018aff09a3851e6f
Matthew Plater <matthewplater2004@gmail.com>
2025-05-02 16:13:03 -0400

    changed DB tables Added 'Staff'

--- database/JJMTTrailDBSetup.sql
+++ database/JJMTTrailDBSetup.sql
@@ -30,6 +30,21 @@ CREATE TABLE IF NOT EXISTS `county_parks`.`Incidents` (
   PRIMARY KEY (`incidentNum`)
 ) ENGINE = InnoDB;
 
+-- -----------------------------------------------------
+-- Table `jjmtdb`.`Staff`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `jjmtdb`.`Staff` (
+  `StaffID` INT NOT NULL AUTO_INCREMENT,
+  `FirstName` VARCHAR(45) NOT NULL,
+  `LastName` VARCHAR(45) NOT NULL,
+  `Email` VARCHAR(45) NULL,
+  `PhoneNumber` VARCHAR(20) NULL,
+  `ParkAssigned` VARCHAR(45) NOT NULL,
+  `Comments` VARCHAR(1000) NULL,
+
+  PRIMARY KEY (`StaffID`)
+) ENGINE = InnoDB;
+
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
@@ -40,3 +55,9 @@ INSERT IGNORE INTO `county_parks`.`Incidents`
 VALUES 
 ('001', 'Cromwell Valley', 'Lime Kiln', 'Joe', '443-555-5555', '2025-01-22 10:22:35', 'Erosion', 'Half of the trail is washed into the stream near the beaver dam', 'New', 'New'),
 ('002', 'Marshy Point', 'White Tail', 'James', '443-555-5555', '2025-03-15 11:45:19', 'Obstruction', 'Fallen Tree', 'New', 'New');
+
+INSERT IGNORE INTO 'county_parks'.'Staff'
+('StaffID', 'FirstName', 'LastName', 'Email', 'PhoneNumber', 'ParkAssigned', 'Comments')
+VALUES
+('0000001', 'John', 'Doe', 'jdoe1@students.towson.edu', '123-456-7890', 'Oregon Ridge', 'This is a test.')
+('0000002', 'Jeffrey', 'Tirschman', 'jtirschman@towson.edu', '410-555-5555', 'Towson Trail', 'Best professor ever!')
